78bac5cba0837d50d33a19e82fbfe69d250b9283
Mario Reder <mario.reder@pm.me>
2026-04-13 09:42:52 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -25,7 +25,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_TELEGRAM_URL": "",
   "VITE_DISCORD_URL": "",
   "VITE_TWITTER_URL": "",
-  "VITE_SEO_SITE_NAME": "",
+  "VITE_SEO_SITE_NAME": "PowerChain",
   "VITE_SEO_SITE_DESCRIPTION": "",
   "VITE_SEO_SITE_URL": "https://dex.orderly.network/powerchain-1870/",
   "VITE_SEO_SITE_LANGUAGE": "en",
